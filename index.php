4e75034d7bdd7815b814bae6ba0113c294066d8e
Michael-Chacon <michaelchacon02@gmail.com>
2022-01-20 22:49:16 -0500

    version funcional de Rango

--- index.php
+++ index.php
@@ -1,6 +1,54 @@
-<?php require_once 'views/head.php';
-require_once 'views/menu.php';?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport"/>
+        <meta content="" name="Peliculas"/>
+        <meta content="" name="Michael"/>
+        <title>
+            Rango
+        </title>
+        <!-- Bootstrap CSS -->
+        <link crossorigin="anonymous" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" rel="stylesheet">
+            <!-- fuente -->
+            <style>
+			@import url('https://fonts.googleapis.com/css2?family=Inconsolata&display=swap');
+			</style>
+            <!-- Iconos do bootstrap-->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css" rel="stylesheet"/>
+            <link href="css/estilos.css" rel="stylesheet">
+                <!-- jquery -->
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js">
+                </script>
+                <!-- ESTILOS  sweet alert -->
+                <link href="librerias/alertas/dist/sweetalert2.min.css" rel="stylesheet"/>
+            </link>
+        </link>
+    </head>
+    <body>
+    	<?php require_once 'views/menu.php'?>
+    	<div class="container-fluid">
+    		<section class="row" id="listadoPeliculas">
+	        	<article class="col-md-2">
+	        		<div class="card">
+					  <img src="images/seven.jpg" class="card-img-top portada" alt="...">
+					  <div class="card-body texto-portada">
+					    <h5 class="card-subtitle mb-2 text-muted text-center">Seven <span class="badge bg-dark">1995</span>
+					    </h5>
+					  </div>
+					  <a href="#" class="stretched-link"></a>
+					</div>
+	        	</article>
+	        </section>
+    	</div>
+    	 <!-- Bootstrap core JS-->
+        <script type="text/javascript" src="js/script.js"></script>
+        <script src="librerias/alertas/dist/sweetalert2.all.min.js">
+        </script>
+       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    </body>
+</html>
 
 
-
-<?php require_once 'views/footer.php';?>
